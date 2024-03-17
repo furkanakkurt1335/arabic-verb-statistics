58f820c86b91660aca609ba3b5b1938532b882fd
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-03-18 00:54:37 +0300

    prune verbs and forms with spaces

--- scripts/prune_verbs.py
+++ scripts/prune_verbs.py
@@ -3,24 +3,24 @@ from pathlib import Path
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--forms', type=str, help='Path to forms.json', required=True)
+    parser.add_argument('-v', '--verbs', type=str, help='Path to verbs.json', required=True)
     return parser.parse_args()
 
 def main():
     args = get_args()
-    forms_path = Path(args.forms)
-    with forms_path.open('r', encoding='utf-8') as f:
+    verbs_path = Path(args.verbs)
+    with verbs_path.open('r', encoding='utf-8') as f:
         verb_d = json.load(f)
     
-    forms_to_delete = []
+    verbs_to_delete = []
     for form, data in verb_d.items():
         if ' ' in form:
-            forms_to_delete.append(form)
+            verbs_to_delete.append(form)
 
-    for form in forms_to_delete:
+    for form in verbs_to_delete:
         del verb_d[form]
 
-    with forms_path.open('w', encoding='utf-8') as f:
+    with verbs_path.open('w', encoding='utf-8') as f:
         json.dump(verb_d, f, ensure_ascii=False, indent=2)
 
 if __name__ == '__main__':
