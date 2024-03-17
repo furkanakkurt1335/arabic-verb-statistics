b65f616fbad429a31e4c1c8cf295b4ccb21150db
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-03-18 01:04:41 +0300

    update count vowel orders

--- scripts/count_vowel_orders.py
+++ scripts/count_vowel_orders.py
@@ -12,16 +12,22 @@ def main():
     with forms_path.open('r', encoding='utf-8') as f:
         verb_d = json.load(f)
     
-    vowels = {
-        chr(1570): 'ā',
-        chr(1614): 'a',
-        chr(1615): 'u',
-        chr(1616): 'i'
-    }
-    fatha = chr(1614)
-    maqsura = chr(1609)
-    alif = chr(1575)
     
+    madda = chr(1570)
+    alif = chr(1575)
+    waw = chr(1608)
+    maqsura = chr(1609)
+    ya = chr(1610)
+    fatha = chr(1614)
+    damma = chr(1615)
+    kasra = chr(1616)
+    vowels = {
+        madda: 'ā',
+        fatha: 'a',
+        damma: 'u',
+        kasra: 'i'
+    }
+
     vowel_d = {}
     for form, data in verb_d.items():
         if 'forms' in data:
@@ -30,25 +36,33 @@ def main():
                 past_original, present_original = form['past']['original'], form['present']['original']
                 past_vowels = []
                 for i, v in enumerate(past_original):
-                    if v == fatha and i < len(past_original) - 1 and (past_original[i + 1] in [maqsura, alif]):
+                    if v == fatha and i < len(past_original) - 1 and past_original[i + 1] in [maqsura, alif]:
                         past_vowels.append('ā')
+                    elif v == kasra and i < len(past_original) - 1 and past_original[i + 1] == ya:
+                        past_vowels.append('ī')
+                    elif v == damma and i < len(past_original) - 1 and past_original[i + 1] == waw:
+                        past_vowels.append('ū')
                     elif v in vowels:
                         past_vowels.append(vowels[v])
                 past_vowels_str = '-'.join(past_vowels)
-                if past_vowels_str == 'a-i-a-ā-a-a-i':
-                    print(form)
                 if past_vowels_str not in vowel_d:
                     vowel_d[past_vowels_str] = {}
                 present_vowels = []
                 for i, v in enumerate(present_original):
                     if v == fatha and i < len(present_original) - 1 and (present_original[i + 1] in [maqsura, alif]):
                         present_vowels.append('ā')
+                    elif v == kasra and i < len(present_original) - 1 and present_original[i + 1] == ya:
+                        present_vowels.append('ī')
+                    elif v == damma and i < len(present_original) - 1 and present_original[i + 1] == waw:
+                        present_vowels.append('ū')
                     elif v in vowels:
                         present_vowels.append(vowels[v])
                 present_vowels_str = '-'.join(present_vowels)
                 if present_vowels_str not in vowel_d[past_vowels_str]:
                     vowel_d[past_vowels_str][present_vowels_str] = 0
                 vowel_d[past_vowels_str][present_vowels_str] += 1
+                if past_vowels_str == 'a-i-a' and present_vowels_str == 'a-u-u':
+                    print(form, past_vowels_str, present_vowels_str)
     
     output_path = Path('data', 'vowel_orders.json')
     with output_path.open('w', encoding='utf-8') as f:
